e440cb1bba2ce89aa2ec629efab6dce00617ecd4
William James Fawcett <william.fawcett@cern.ch>
2021-03-01 20:48:24 +0000

    remove pdflatex dependency

--- latex2table.rb
+++ latex2table.rb
@@ -5,8 +5,6 @@ class Latex2table < Formula
   sha256 "0759e7233373c35ac8ee8f471c2a19a9042a8108b3a223af063537ab23f0772f"
   license "ISC"
 
-  depends_on "pdflatex"
-
   def install
         bin.install "latex2table" 
   end
