7a018f786a0980901e94f44c35c1313206062f0c
William James Fawcett <william.fawcett@cern.ch>
2021-03-01 19:44:59 +0000

    add formula

--- latex2table.rb
+++ latex2table.rb
@@ -1,6 +1,3 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Latex2table < Formula
   desc "Create a pdf of a table from a latex table fragment"
   homepage ""
@@ -8,28 +5,8 @@ class Latex2table < Formula
   sha256 "6402112957e52531dd0006270665a336ba35162c3e2059ad5075a2e22abadab1"
   license "ISC"
 
-  # depends_on "cmake" => :build
-
   def install
-    # ENV.deparallelize  # if your formula fails when building in parallel
-    # Remove unrecognized options if warned by configure
-    system "./configure", "--disable-debug",
-                          "--disable-dependency-tracking",
-                          "--disable-silent-rules",
-                          "--prefix=#{prefix}"
-    # system "cmake", ".", *std_cmake_args
+        bin.install "testscript" 
   end
 
-  test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test latex2table`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system "false"
-  end
 end
