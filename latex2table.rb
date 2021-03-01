e4c6d488d601730b4db4d9cc9afc69124c9f247d
William James Fawcett <william.fawcett@cern.ch>
2021-03-01 21:19:32 +0000

    Update to v1.1.0

--- latex2table.rb
+++ latex2table.rb
@@ -1,12 +1,13 @@
 class Latex2table < Formula
   desc "Create a pdf of a table from a latex table fragment."
   homepage "https://github.com/will-fawcett/latex2table"
-  url "https://github.com/will-fawcett/latex2table/archive/v1.0.0.tar.gz"
-  sha256 "0759e7233373c35ac8ee8f471c2a19a9042a8108b3a223af063537ab23f0772f"
+  url "https://github.com/will-fawcett/latex2table/archive/v1.1.0.tar.gz"
+  sha256 "34712a7de96b4997e1cb281039ceead32ea93d663eb4f417f3040acb65cd7677"
   license "ISC"
 
   def install
         bin.install "latex2table" 
+        bin.install "make_table.tex"
   end
 
 end
